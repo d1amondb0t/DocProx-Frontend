07cdec50197c1098b366385f32870a8f8cfc2ea4
d1amondb0t <d1amondb0t@outlook.com>
2023-09-01 08:33:55 -0400

    [update] - Adding necessary files, css start

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <ul>
+        <p>test</p>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Logout</li>
+        <li>Options</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
